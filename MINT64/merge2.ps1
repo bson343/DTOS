6490ec2504c43e5494773ec1517bfb3aa48aed29
bson343 <byeonggukson@gmail.com>
2024-07-23 19:47:12 +0900

    실행파일을 합치는 로직 개선

---
+++ MINT64/merge2.ps1
@@ -0,0 +1,36 @@
+param(
+    [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
+    [string[]]$InputArray=@()
+)
+
+Write-Host "Check Input String"
+
+foreach($element in $InputArray)
+{
+    Write-Host $element
+}
+
+$length = $InputArray.Count
+Write-Host "InputCount: $length"
+
+$DestinationFile = "Disk.img";
+
+# 입력 파일들이 존재하는지 확인
+foreach ($file in $InputArray) {
+    if (-Not (Test-Path $file)) {
+        Write-Host "not exist files: $file"
+        exit 1
+    }
+}
+
+# 출력 파일이 이미 존재하면 삭제
+if (Test-Path $DestinationFile) {
+    Remove-Item $DestinationFile
+}
+
+# 각 파일의 내용을 출력 파일로 합침
+foreach ($file in $InputArray) {
+    Get-Content $file -Encoding Byte | Add-Content -Path $DestinationFile -Encoding Byte
+}
+
+Write-Host "File merging completed. Result FileName: $DestinationFile"
