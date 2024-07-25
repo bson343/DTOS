8901fa5a1d136fe24e71ae0c0b2d6f62ea6b7ea5
bson343 <byeonggukson@gmail.com>
2024-07-25 17:42:17 +0900

    보호 모드의 C커널에서 IA-32e모드 실행을 위한 준비 코드 작성
    1. 메모리 용량 확인
    2. IA-32e 모드 영역(1MB ~ 6MB) 초기화

--- MINT64/01.Kernel32/Source/Main.c
+++ MINT64/01.Kernel32/Source/Main.c
@@ -9,6 +9,8 @@
 #include "Types.h"
 
 void kPrintString( int iX, int iY, const char* pcString );
+BOOL kInitializeKernel64Area( void );
+BOOL kIsMemoryEnough( void );
 
 /**
  *  아래 함수는 C 언어 커널의 시작 부분임
@@ -17,7 +19,31 @@ void kPrintString( int iX, int iY, const char* pcString );
 void Main( void )
 {
     //__asm__ ("int3"); 제어권 테스트
-    kPrintString( 0, 3, "C Language Kernel Started~!!!" );
+    kPrintString( 0, 3, "C Language Kernel Start.....................[Pass]" );
+
+
+    // 최소 메모리 크기를 만족하는 지 검사
+    kPrintString( 0, 4, "Minimum Memory Size Check...................[    ]" );
+    if (kIsMemoryEnough() == FALSE)
+    {
+        kPrintString( 45, 4, "Fail" );
+        kPrintString( 0, 5, "Not Enough Memory~!! MINT64 OS Requires Over 64Mbyte Memory~!!");
+        while( 1 ) ;
+    }
+    else
+    {
+        kPrintString( 45, 4, "Pass" );
+    }
+
+    // IA-32e 모드의 커널 영역을 초기화
+    kPrintString(0, 5, "IA-32e Kernel Area Initialize...............[    ]");
+    if( kInitializeKernel64Area() == FALSE )
+    {
+        kPrintString( 45, 5, "Fail" );
+        kPrintString( 0, 6, "Kernel Area Initialization Fail~!!" );
+        while( 1 ) ;
+    }
+    kPrintString( 45, 5, "Pass" );
 
     while( 1 ) ;
     
@@ -39,5 +65,59 @@ void kPrintString( int iX, int iY, const char* pcString )
     {
         pstScreen[ i ].bCharactor = pcString[ i ];
     }
+}
+
+// IA-32e 모드용 커널 영역을 0으로 초기화(1MB ~ 6MB)
+BOOL kInitializeKernel64Area( void )
+{
+    DWORD* pdwCurrentAddress;
+
+    // 초기화를 시작할 주소인 0x100000(1MB)을 설정
+    pdwCurrentAddress = (DWORD*) 0x100000;
+
+    // 포인터의 주소와 숫자를 비교하면 경고가 뜨며, 
+    // 숫자 비교를 할 것이라고 명시적으로 표현하기 위해 수자 자료형으로 캐스팅 후 비교
+    while ( (DWORD) pdwCurrentAddress < 0x600000)
+    {
+        *pdwCurrentAddress = 0x00;
+
+        // 0으로 저장한 수 다시 읽었을 때 0이 나오지 않으면 해당 어드레스를
+        // 사용하는데 문제가 생긴 것이므로 더이상 진행하지않고 종료
+        if (*pdwCurrentAddress != 0)
+        {
+            return FALSE;
+        }
+
+        pdwCurrentAddress++;
+    }
+
+    return TRUE;
+}
+
+// MINT64 OS를 실행하기에 충분한 메모리를 가지고 있는지 체크
+// 검사 알고리듬: 특정 주소에 지정한 값을 쓰고 읽었는때 정상적으로 읽을 수 있는지 확인
+BOOL kIsMemoryEnough( void )
+{
+    DWORD* pdwCurrentAddress;
+
+    // 0x100000(1MB)부터 검사 시작
+    pdwCurrentAddress = (DWORD*) 0x100000;
+
+    // 0x4000000(64MB)까지 루프를 돌면서 확인
+    while ( ( DWORD ) pdwCurrentAddress < 0x4000000 )
+    {
+        *pdwCurrentAddress = 0x12345678;
+
+        // 0x12345678로 저장한 후 다시 읽었을 때 0x12345678이 나오지 않으면 
+        // 해당 어드레스를 사용하는데 문제가 생긴 것이므로 더이상 진행하지 않고 종료
+        if( *pdwCurrentAddress != 0x12345678 )
+        {
+           return FALSE;
+        }
+
+        // 1MB씩 이동하면서 확인
+        pdwCurrentAddress += ( 0x100000 / 4 );  // 포인터 대상의 타입크기가 DWORD(4Byte)이다.
+    }
+    return TRUE;
 }
 
