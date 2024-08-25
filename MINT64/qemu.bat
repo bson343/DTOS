ddfa128242c47eebf80f3f3c188a0598bc7cbba5
bson343 <byeonggukson@gmail.com>
2024-08-25 14:57:24 +0900

    qemu 실행에 문제가 생겨서 버전을 변경함

--- MINT64/qemu.bat
+++ MINT64/qemu.bat
@@ -1,1 +1,5 @@
-qemu-system-x86_64.exe -m 64 -fda c:/Users/SonByeongguk/WT/DTOS/MINT64/Disk.img -rtc base=localtime -M pc
+@REM qemu 버전이 높은지 실습에 문제가 생김, 책이 안내한 버전인 qemu_0.10.04.bat을 실행할 것
+@echo Due to an unidentified issue caused by a version discrepancy from the book, which has critically affected the practice, please run qemu_0.10.04.bat!
+
+exit
+qemu-system-x86_64.exe -m 1024 -fda c:/Users/SonByeongguk/WT/DTOS/MINT64/Disk.img -display sdl -rtc base=localtime -M pc 
---
+++ MINT64/qemu_0.10.04.bat
@@ -0,0 +1,3 @@
+set myPath=C:\Users\SonByeongguk\qemu-0.10.4
+
+%myPath%\qemu-system-x86_64.exe -L %myPath% -m 1024 -fda c:/Users/SonByeongguk/WT/DTOS/MINT64/Disk.img -localtime -M pc
