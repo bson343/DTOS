4b9aaa10298fd7b9d33f3c58ea98d5d4c40d562d
bson_HPLAPTOP <bson7596@gmail.com>
2025-03-01 19:01:47 +0900

    refactor: 경로 환경변수로 대체체

--- MINT64/qemu_0.10.04.bat
+++ MINT64/qemu_0.10.04.bat
@@ -1,3 +1,3 @@
-set myPath=C:\Users\SonByeongguk\qemu-0.10.4
+set myPath=%HOME%\qemu-0.10.4
 
-%myPath%\qemu-system-x86_64.exe -L %myPath% -m 1024 -fda c:/Users/SonByeongguk/WT/DTOS/MINT64/Disk.img -localtime -M pc
+%myPath%\qemu-system-x86_64.exe -L %myPath% -m 1024 -fda %HOME%/WT/DTOS/MINT64/Disk.img -boot a -localtime -M pc
